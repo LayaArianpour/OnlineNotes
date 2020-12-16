7fa6ea723aa2b240c46d951b12a4c12dd20624ed
laya <l.aryanpour80@gmail.com>
2020-12-16 14:26:33 +0330

    Part2: Design UI

---
+++ app/src/main/java/com/example/onlinenotes/ContentNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.onlinenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContentNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_note);
+    }
+}
